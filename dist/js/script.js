840918149fc50ac8cd0cf03bd958a60973079549
Yehor <yehor1koval@gmail.com>
2022-07-14 21:07:47 +0300

    End first screen

---
+++ dist/js/script.js
@@ -0,0 +1,12 @@
+const hamburger = document.querySelector('.hamburger')
+const menu = document.querySelector('.menu')
+const close = document.querySelector('.menu__close');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.add('active');
+});
+
+close.addEventListener('click', () => {
+    menu.classList.remove('active');
+});
+
